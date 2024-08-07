965dbbbf8011723be268b74674dfa8e99e1f07fc
xola-mthembu <xola.mthembu@gmail.com>
2024-08-07 18:03:16 +0000

    Task 7: Create stored procedure ComputeAverageScoreForUser

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,14 @@
+-- Task 7: Create stored procedure ComputeAverageScoreForUser
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+	DECLARE avg_score FLOAT;
+	
+	SELECT AVG(score) INTO avg_score
+	FROM corrections
+	WHERE user_id = user_id;
+	
+	UPDATE users SET average_score = avg_score WHERE id = user_id;
+END;
+//
+DELIMITER ;
