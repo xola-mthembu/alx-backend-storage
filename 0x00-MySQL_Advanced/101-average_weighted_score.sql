14b18c14cb596b9ca5b2c2f8eaeca6413c1deb40
xola-mthembu <xola.mthembu@gmail.com>
2024-08-07 18:11:12 +0000

    Task 13: Average weighted score for all!

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,25 +1,13 @@
--- Task 13: Create stored procedure ComputeAverageWeightedScoreForUsers
+-- Create stored procedure ComputeAverageWeightedScoreForUsers
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-	DECLARE user_cursor CURSOR FOR SELECT id FROM users;
-	DECLARE done INT DEFAULT FALSE;
-	DECLARE user_id INT;
-	
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-	
-	OPEN user_cursor;
-	
-	read_loop: LOOP
-		FETCH user_cursor INTO user_id;
-		IF done THEN
-			LEAVE read_loop;
-		END IF;
-		
-		CALL ComputeAverageWeightedScoreForUser(user_id);
-	END LOOP;
-	
-	CLOSE user_cursor;
-END;
-//
+    UPDATE users u
+    SET average_score = (
+        SELECT IFNULL(SUM(c.score * p.weight) / SUM(p.weight), 0)
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        WHERE c.user_id = u.id
+    );
+END //
 DELIMITER ;
