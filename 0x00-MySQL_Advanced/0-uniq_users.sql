b39874f66957ffa349a966e69e0bae382c9bb566
xola-mthembu <xola.mthembu@gmail.com>
2024-08-07 17:49:16 +0000

    Task 0: Create users table with unique email

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,6 @@
+-- Task 0: Create users table with unique email
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
