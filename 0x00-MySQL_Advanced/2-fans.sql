22b60103f3edd0e1b875b2d71d2c0e3d6ac4b38d
xola-mthembu <xola.mthembu@gmail.com>
2024-08-07 18:15:37 +0000

    Task 2: Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Task 2: Rank country origins of bands by the number of fans
-SELECT origin, SUM(nb_fans) as nb_fans
+SELECT origin, SUM(fans) as nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
